f9b8b7b84cb2f957ec9bd0667062022104cafa2e
theOneSun <sunjian_better@163.com>
2018-12-11 14:13:13 +0800

    init project

---
+++ src/main/java/com/sj/athletic/meeting/AthleticMeetingApplication.java
@@ -0,0 +1,12 @@
+package com.sj.athletic.meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleticMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthleticMeetingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sj/athletic/meeting/AthleticMeetingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sj.athletic.meeting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AthleticMeetingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
